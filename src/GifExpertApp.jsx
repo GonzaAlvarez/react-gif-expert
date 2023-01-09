4b5106e1f8164af3580ccb0bebb6e84515358de1
GonzaAlvarez <gonzaalvarezz@hotmail.com>
2023-01-09 13:36:30 -0300

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Messi']);
+
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+
+        setCategories([newCategory, ...categories]);
+    }
+
+    return (
+        <>
+            {/* Titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+            <AddCategory
+                onNewCategory={(event) => onAddCategory(event)}
+            />
+
+            {/* Listado de Gif */}
+            {
+                categories.map(category => (
+                    <GifGrid
+                        key={category}
+                        category={category}
+                    />
+                ))
+            }
+            {/* Gif Item */}
+        </>
+    )
+}
